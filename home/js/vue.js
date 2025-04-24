27c1aa52dec2c4ad3411f7a96371f1445b9ec14a
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-04-24 11:06:12 +0800

    Added vbind directive

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,7 +1,9 @@
   const app = Vue.createApp({
    data() {
     return {
-     message: "Hello WEBPROG AFD241 World! With separate CSS and JS files"
+     message: "Hello WEBPROG AFD241 World! With separate CSS and JS files",
+     vueClass: "pinkBG"
+
     }
    }
   })
