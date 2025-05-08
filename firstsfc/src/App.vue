134b77f958e6dfd34258ff63236fcc46f4de4d75
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-05-08 10:45:59 +0800

    added FoodItem2 component

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -2,10 +2,7 @@
   <h1>Hello WEBPROG AFD241 World!</h1>
   <h2>Food</h2>
   <food-item/>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-
+  <food-item2/> 
 </template>
 
 <script></script>
--- firstsfc/src/components/FoodItem2.vue
+++ firstsfc/src/components/FoodItem2.vue
@@ -9,8 +9,8 @@
   export default {
     data() {
       return {
-        name: 'Apples',
-        message: 'I like apples'
+        name: 'Oranges',
+        message: 'I like oranges too'
       }
     }
   };
@@ -22,6 +22,6 @@
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: lightgreen;
+    background-color: orange;
   }
 </style>
